8b53bfaf2eccd8eea9239bf1ab1a173472146d78
femilana <phemeelana@gmail.com>
2024-03-12 08:27:05 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last_number = number % -(10)
+        print(-(last_number), end='')
+    else:
+        last_number = number % 10
+        print(last_number, end='')
+    return abs(last_number)
