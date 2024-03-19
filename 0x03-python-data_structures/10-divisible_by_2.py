4a2a8b5aa14f6db402fb8b47c1430569c38de212
femilana <phemeelana@gmail.com>
2024-03-18 21:19:40 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = []
+    if my_list:
+        for number in my_list:
+            new.append(False if number % 2 else True)
+        return new_list
